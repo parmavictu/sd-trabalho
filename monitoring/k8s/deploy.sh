840df4f58d5c0e7eb0782b83ccc21d6e8f383f98
Rafael Geraldo de Jesus Nalin <CIANDT\rafaelnalin@lnb024563cps.ciandt.global>
2022-09-24 18:39:01 -0300

    Add API's NodeJS

--- monitoring/k8s/deploy.sh
+++ monitoring/k8s/deploy.sh
@@ -5,30 +5,21 @@ _ROOT_PATH=$(echo $(ls -l /proc/$$/fd | egrep 'deploy.sh' | grep -Po '(?<=-> ).*
 _WHERE_AIM=$(pwd)
 
 cd $_ROOT_PATH
-source ./../../sh/utils.sh
-source ./../../environment/k8s/.custom.env
-
-LoggerInfo 'Iniciando monitoramento...'
-
 kubectl delete -f monitoring-namespace.yaml
 kubectl create -f monitoring-namespace.yaml
 
 sleep 5
 
-LoggerInfo 'Realizando deploy: prometheus'
 kubectl create -f prometheus-config.yaml
 kubectl create -f prometheus-deployment.yaml
 kubectl create -f prometheus-service.yaml
 
 # view via CLI:
-LoggerInfo 'Verificando services...'
 kubectl get services --namespace=monitoring
 
-LoggerInfo 'Verificando deployments...'
 kubectl get deployments --namespace=monitoring
 
 # show the dashboard
-LoggerInfo 'Abrindo dashboard...'
 minikube dashboard &
 
 # click around
@@ -37,15 +28,11 @@ minikube dashboard &
 # show graph and query of container_memory_usage_bytes{kubernetes_namespace="monitoring"}
 
 # deploy grafana
-LoggerInfo 'Realizando deploy: grafana'
 kubectl create -f grafana-deployment.yaml
 kubectl create -f grafana-ingress.yaml
 
 # show grafana
-LoggerInfo 'Verificando services...'
 kubectl get services --namespace=monitoring
-
-LoggerInfo 'Verificando deployments...'
 kubectl get deployments --namespace=monitoring
 
 # add datasource. make sure type is prometheus http://prometheus:8080
@@ -57,7 +44,6 @@ kubectl get deployments --namespace=monitoring
 
 # lets add node metrics
 # deploy node exporter. explain daemonser
-LoggerInfo 'Realizando deploy: node-exporter'
 kubectl create -f node-exporter-daemonset.yml
 
 # show new target in prometheus. explain it autodiscovering the pods
